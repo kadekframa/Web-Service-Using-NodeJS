aaab6ad3a066c77754da3a93e7f305c7a8707af7
kadekframa <dframa19@gmail.com>
2022-04-22 01:36:55 +0800

    Trainning Web Service. Response Status

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const http = require('http');
  
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
- 
-    response.statusCode = 200;
 
     const { url, method } = request;
 
     if(url === '/'){
         // todo 2
         if(method === 'GET'){
+            response.statusCode = 200;
             response.end('Ini adalah homepage');
         }else{
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${request.method} request`);
         }
 
     }else if(url === '/about'){
         // todo 3
         if(method === 'GET'){
+            
+            response.statusCode = 200;
             response.end('Hallo! Ini adalah halaman about');
         }else if(method === 'POST'){
         let body = [];
@@ -29,13 +31,19 @@ const requestListener = (request, response) => {
         request.on('end', () => {
             body = Buffer.concat(body).toString();
             const { name } = JSON.parse(body);
+            
+            response.statusCode = 200;
             response.end(`Hallo, ${name}!`);
         });
         }else{
+            
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request`);
         }
     }else{
         // todo 1
+        
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
